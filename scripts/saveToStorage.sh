adf09e5dd219bda555524ddc78af4de8d2089b1c
yuvalHelman <yuvalasaf3@gmail.com>
2019-05-27 14:01:49 +0300

    script edit

--- scripts/saveToStorage.sh
+++ scripts/saveToStorage.sh
@@ -2,4 +2,4 @@
 
 dir_num=$(ls -l | wc -l)
 
-cp -R {$HOME}/code/MLApplicationsForGraphics/no_git/models /cloudstorage/yuvalHelman/savedModels/models${dir_num}/
+cp -R ~/code/MLApplicationsForGraphics/no_git/models /cloudstorage/yuvalHelman/savedModels/models${dir_num}/
--- src/frogsDataset.py
+++ src/frogsDataset.py
@@ -1,5 +1,4 @@
 import os
-
 import skimage.io
 import torch
 from skimage.color import rgba2rgb
