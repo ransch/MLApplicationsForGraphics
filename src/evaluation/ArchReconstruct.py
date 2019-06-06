7d1fc2fa5e47a3ec51291e5fa4f476a03adf89ef
yuvalHelman <yuvalasaf3@gmail.com>
2019-06-06 16:05:25 +0300

    added TODO reconstruction of image in our arch

---
+++ src/evaluation/ArchReconstruct.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torchvision.utils as vutils
+
+from src import hyperparameters as hyperparams
+from src import settings
+from src.frogsDataset import FrogsDataset as Dataset
+from src.networks.generator import Generator
+from src.utils import findOptimalLatentVector
+
+
+def main():
+    index = 18
+    # TODO: do this!
+
+if __name__ == '__main__':
+    main()
